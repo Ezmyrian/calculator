6b5732915026ffa5b88af2438624663e6463accb
ezmyrian <josh.ray1284@gmail.com>
2022-02-24 17:26:57 -0600

    Rename basic functions, Add function to take user input and function to evaluate.

--- script.js
+++ script.js
@@ -1,33 +1,293 @@
-function add(a, b) {
-    let add = a + b;
-    console.log(add);
+function addTogether(a, b) {
+    let add = +a + +b;
+    return add;
 }
 
-function subtract(a, b) {
+function subtractFrom(a, b) {
     let subtract = a - b;
-    console.log(subtract);
+    return subtract;
 }
 
-function multiply(a, b) {
+function multiplyBy(a, b) {
     let multiple = a * b;
-    console.log(multiple);
+    return multiple;
 }
 
-function divide(a, b) {
+function divideBy(a, b) {
     if (b == 0) {
         alert('Your attempt to divide by 0 has caused a black hole that has devoured the universe and everyone in it.  Please refrain from further universe destruction.');
         return;
     }
     division = a / b;
-    console.log(division);
+    return division;
 }
 
 function operate(num1, num2, operator) {
-    let solution = (operator == '*') ? multiply(num1, num2)
-        : (operator == '/') ? divide(num1, num2)
-        : (operator == '+') ? add(num1, num2)
-        : (operator == '-') ? subtract(num1, num2)
+    let solve = (operator == '*') ? multiplyBy(num1, num2)
+        : (operator == '/') ? divideBy(num1, num2)
+        : (operator == '+') ? addTogether(num1, num2)
+        : (operator == '-') ? subtractFrom(num1, num2)
         : alert('Error in operate function');
-    return solution;
+    return solve;
+}
+
+function operateValues(displayArray) {
+    if (displayArray.length < 3) {
+        alert('Please use at least two numbers with a valid operator');
+        return;
+    }
+    let firstValue = displayArray[0];
+    let operand = displayArray[1];
+    let secondValue = displayArray[2];
+    let finalValue = operate(firstValue, secondValue, operand);
+    if (displayArray.length == 3) {
+        return finalValue;
+    }
+    for (i = 2; i < (displayArray.length - 2); i++) {
+        firstValue = finalValue;
+        operand = displayArray[i+1];
+        secondValue = displayArray[i+2];
+        finalValue = operate(firstValue, secondValue, operand);
+        i++;
+    }
+    return finalValue;
+}
+
+function convertArray(array) {
+    let arrayString = '';
+    for (i = 0; i < array.length; i++) {
+        arrayString += array[i];
+    }
+    console.log(arrayString);
+    return arrayString;
+}
+
+
+const displaySelector = document.querySelector('.display');
+
+const buttons = document.querySelectorAll('[id]');
+for (i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function() {
+        buttonClicked(this.id);
+    });
+};
+
+let display = '';
+let solution = 'empty';
+const displayArray = [];
+
+function buttonClicked(id) { 
+    switch (id) {
+        //case 'backspace':
+        
+        //    break;
+
+        case 'clear':
+            let loopLength = displayArray.length;
+            for (i = 0; i < loopLength; i++) {
+                displayArray.pop();
+            }
+            display = '';
+            solution = 'empty';
+            displaySelector.textContent = '0';
+            break;
+
+        case 'seven':
+            display += `7`;
+            if (displayArray.length) {
+                displaySelector.textContent = convertArray(displayArray) + display;
+            }
+            else {
+                displaySelector.textContent = display;
+            }
+            break;
+
+        case 'eight':
+            display += `8`
+            if (displayArray.length) {
+                displaySelector.textContent = convertArray(displayArray) + display;
+            }
+            else {
+                displaySelector.textContent = display;
+            }
+            break;
+
+        case 'nine':
+            display += `9`
+            if (displayArray.length) {
+                displaySelector.textContent = convertArray(displayArray) + display;
+            }
+            else {
+                displaySelector.textContent = display;
+            }
+            break;
+
+        case 'divide':
+            if (solution != 'empty') {
+                displayArray.push(solution);
+                display = solution;
+            }
+            if (display == '') {
+                return;
+            }
+            
+            if (display != solution) {
+                displayArray.push(display);
+            }
+            solution = 'empty';
+            displayArray.push('/');
+            displaySelector.textContent = convertArray(displayArray);
+            display = '';
+            break;
+
+        case 'four':
+            display += `4`
+            if (displayArray.length) {
+                displaySelector.textContent = convertArray(displayArray) + display;
+            }
+            else {
+                displaySelector.textContent = display;
+            }
+            break;
+
+        case 'five':
+            display += `5`
+            if (displayArray.length) {
+                displaySelector.textContent = convertArray(displayArray) + display;
+            }
+            else {
+                displaySelector.textContent = display;
+            }
+            break;
+
+        case 'six':
+            display += `6`
+            if (displayArray.length) {
+                displaySelector.textContent = convertArray(displayArray) + display;
+            }
+            else {
+                displaySelector.textContent = display;
+            }
+            break;
+
+        case 'multiply':
+            if (solution != 'empty') {
+                displayArray.push(solution);
+                display = solution;
+            }
+            if (display == '') {
+                return;
+            }
+            
+            if (display != solution) {
+                displayArray.push(display);
+            }
+            solution = 'empty';
+            displayArray.push('*');
+            displaySelector.textContent = convertArray(displayArray);
+            display = '';
+            break;
+
+        case 'one':
+            display += `1`
+            if (displayArray.length) {
+                displaySelector.textContent = convertArray(displayArray) + display;
+            }
+            else {
+                displaySelector.textContent = display;
+            }
+            break;
+
+        case 'two':
+            display += `2`
+            if (displayArray.length) {
+                displaySelector.textContent = convertArray(displayArray) + display;
+            }
+            else {
+                displaySelector.textContent = display;
+            }
+            break;
+
+        case 'three':
+            display += `3`
+            if (displayArray.length) {
+                displaySelector.textContent = convertArray(displayArray) + display;
+            }
+            else {
+                displaySelector.textContent = display;
+            }
+            break;
+
+        case 'subtract':
+            if (solution != 'empty') {
+                displayArray.push(solution);
+                display = solution;
+            }
+            if (display == '') {
+                return;
+            }
+            
+            if (display != solution) {
+                displayArray.push(display);
+            }
+            solution = 'empty';
+            displayArray.push('-');
+            displaySelector.textContent = convertArray(displayArray);
+            display = '';
+            break;
+
+        case 'zero':
+            display += `0`
+            if (displayArray.length) {
+                displaySelector.textContent = convertArray(displayArray) + display;
+            }
+            else {
+                displaySelector.textContent = display;
+            }
+            break;
+
+        //case 'decimal':
+
+        //    break;
+
+        case 'equal':
+            if (display == '') {
+                return;
+            }
+            displayArray.push(display);
+            solution = operateValues(displayArray);
+            display = solution;
+            displaySelector.textContent = solution;
+            let loopsLength = displayArray.length;
+            for (i = 0; i < loopsLength; i++) {
+                displayArray.pop();
+            }
+            break;
+        
+        case 'add':
+            if (solution != 'empty') {
+                displayArray.push(solution);
+                display = solution;
+                console.log('display != empty', display);
+            }
+            if (display == '') {
+                return;
+            }
+            
+            if (display != solution) {
+                displayArray.push(display);
+                console.log('display != solution', display);
+            }
+            solution = 'empty';
+            displayArray.push('+');
+            console.log('displayArray', displayArray);
+            displaySelector.textContent = convertArray(displayArray);
+            console.log('displaySelector', displaySelector.textContent);
+            display = '';
+            break;
+
+        default:
+            console.log('Error in buttonClicked function: ', id);  
+    }
 }
 
