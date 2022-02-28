345b92fe8276ecb678aac384d60cd27d91e71efd
ezmyrian <josh.ray1284@gmail.com>
2022-02-27 23:06:04 -0600

    Update operate function, Delete individual operation functions
    Move functionality from buttonClicked switch to dedicated functions
    and use function calls inside switch instead.

--- script.js
+++ script.js
@@ -1,46 +1,3 @@
-function addTogether(a, b) {
-    return a + b;
-}
-
-function subtractFrom(a, b) {
-    return a - b;
-}
-
-function multiplyBy(a, b) {
-    return a * b;
-}
-
-function divideBy(a, b) {
-    if (b == 0) {
-        alert('Your attempt to divide by 0 has caused a black hole that has devoured the universe and everyone in it.  Please refrain from further universe destruction.');
-        return;
-    }
-    return a / b; 
-}
-
-function operate(num1, num2, operator) {
-    switch (operator) {
-        case '*':
-            return multiplyBy(num1, num2);
-        case '/':
-            return divideBy(num1, num2);
-        case '+':
-            return addTogether(num1, num2);
-        case '-':
-            return subtractFrom(num1, num2);
-        default:
-            alert('Error in operate function');
-            break;
-    }
-}
-
-function operateOnValues(numberAndOperatorInput) {
-    let firstValue = Number(numberAndOperatorInput.firstNumber);
-    let secondValue = Number(numberAndOperatorInput.secondNumber);
-    let operand = numberAndOperatorInput.operator;
-    return  Number(operate(firstValue, secondValue, operand).toFixed(2));
-}
-
 const displayValue = document.querySelector('.display');
 const equalButton = document.querySelector('#equal');
 const backspaceButton = document.querySelector('#backspace');
@@ -54,118 +11,158 @@ numberAndOperatorInput.operator = '';
 numberAndOperatorInput.secondNumber = '';
 
 let userNumberInput = '';
+//let newOperation = true;
+
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case '*':
+            return num1 * num2;
+        case '/':
+            if (num2 == 0) {
+                alert('Your attempt to divide by 0 has caused a black hole that has devoured the universe and everyone in it.  Please refrain from further universe destruction.');
+                return;
+            }
+            return num1 / num2;
+        case '+':
+            return num1 + num2;
+        case '-':
+            return num1 - num2;
+        default:
+            alert('Error in operate function');
+            break;
+    }
+}
+
+function operateOnValues(numberAndOperatorInput) {
+    let firstValue = Number(numberAndOperatorInput.firstNumber);
+    let secondValue = Number(numberAndOperatorInput.secondNumber);
+    let operand = numberAndOperatorInput.operator;
+    return  Number(operate(firstValue, secondValue, operand).toFixed(2));
+}
 
 function buttonClicked(event) { 
-    let eventTarget = event.target.value;
-    let result = 0;
-    switch (eventTarget) {
+    let buttonValue = event.target.value;
+    switch (buttonValue) {
         case '+':
         case '-':
         case '*': 
         case '/':
-            if (userNumberInput == '' && numberAndOperatorInput.firstNumber != '') {
-                userNumberInput = numberAndOperatorInput.firstNumber;
-                numberAndOperatorInput.firstNumber = '';
-            }
-            if (userNumberInput == '' || userNumberInput == '.') {
-                return;
-            }
-            if (numberAndOperatorInput.firstNumber == '') {
-                numberAndOperatorInput.firstNumber = userNumberInput;
-                userNumberInput = '';
-            }
-            if (numberAndOperatorInput.operator == '') {
-                numberAndOperatorInput.operator = eventTarget;
-            }    
-            displayValue.textContent = Object.values(numberAndOperatorInput).join(' ');       
-            if (numberAndOperatorInput.firstNumber == '' || 
-                numberAndOperatorInput.operator == '' ||
-                userNumberInput == '') {
-                    return;
-            }
-            else {
-                let equal = new MouseEvent('click', {bubbles: true});
-                equalButton.dispatchEvent(equal);
-                numberAndOperatorInput.operator = eventTarget;
-            }
+            symbol(buttonValue);
             break;   
-
         case 'backspace':
-            //when using backspace on an operator, user can then input another number, and then when
-            //clicking an operator, the function evaluates.  Fix this to not allow number input until
-            //another operator is pressed, followed by another number.
-            if (userNumberInput != '') {
-                userNumberInput = userNumberInput.toString().slice(0, -1);
-                if (Object.values(numberAndOperatorInput).every(value => value == '')) {
-                    displayValue.textContent = userNumberInput;
-                }
-                else {
-                    displayValue.textContent = `${numberAndOperatorInput.firstNumber} ${numberAndOperatorInput.operator} ${userNumberInput}`
-                }
-                return;
-            }
-            else if (numberAndOperatorInput.operator != '') {
-                numberAndOperatorInput.operator = '';
-                userNumberInput = numberAndOperatorInput.firstNumber
-                numberAndOperatorInput.firstNumber = '';
-                displayValue.textContent = userNumberInput;
-                return;
-            }
-            else if (userNumberInput == '' && numberAndOperatorInput.operator == '') {
-                userNumberInput = numberAndOperatorInput.firstNumber;
-                numberAndOperatorInput.firstNumber = '';
-                let backspace = new MouseEvent('click', {bubbles: true});
-                backspaceButton.dispatchEvent(backspace);
-            }
-
-            break;
-    
+            backspace();
+            break;   
         case 'clr':
-            displayValue.textContent = '0';
-            numberAndOperatorInput.firstNumber = '';
-            numberAndOperatorInput.secondNumber = '';
-            numberAndOperatorInput.operator = '';
-            userNumberInput = '';
-            break;
-            
+            clear();
+            break;            
         case '.':
-            if (userNumberInput.includes('.') || displayValue.textContent.includes('.')) {
-                alert('Number already contains a decimal');
-                return;
-            }
-            userNumberInput += '.';
-            displayValue.textContent += '.';
-            break;
-        
+            decimal();
+            break;        
         case '=':
-            if (numberAndOperatorInput.firstNumber == '' || 
-                numberAndOperatorInput.operator == '' ||
-                userNumberInput == '' || userNumberInput == '.') {
-                    return;
-            }
-            else {
-                numberAndOperatorInput.secondNumber = userNumberInput;
-                result = operateOnValues(numberAndOperatorInput);
-                displayValue.textContent = result;
-                numberAndOperatorInput.firstNumber = result;
-                numberAndOperatorInput.secondNumber = '';
-                numberAndOperatorInput.operator = '';
-                userNumberInput = '';
-            }
-            break;
-            
+            equal();
+            break;            
         default:
-            if (userNumberInput == '' && numberAndOperatorInput.operator == '' && numberAndOperatorInput.firstNumber != '') {
-                userNumberInput = numberAndOperatorInput.firstNumber;
-                numberAndOperatorInput.firstNumber = '';
-            }
-            userNumberInput += eventTarget;
-            if (Object.values(numberAndOperatorInput).every(value => value == '')) {
-                displayValue.textContent = userNumberInput;
-            }
-            else {
-                displayValue.textContent = `${numberAndOperatorInput.firstNumber} ${numberAndOperatorInput.operator} ${userNumberInput}`
-            }
+            numberInput(buttonValue);
             break; 
+    }
+}
+
+function symbol(buttonValue) {
+    if (userNumberInput == '' && numberAndOperatorInput.firstNumber != '') {
+        userNumberInput = numberAndOperatorInput.firstNumber;
+        numberAndOperatorInput.firstNumber = '';
+    }
+    if (userNumberInput == '' || userNumberInput == '.') {
+        return;
+    }
+    if (numberAndOperatorInput.firstNumber == '') {
+        numberAndOperatorInput.firstNumber = userNumberInput;
+        userNumberInput = '';
+    }
+    if (numberAndOperatorInput.operator == '') {
+        numberAndOperatorInput.operator = buttonValue;
+    }    
+    displayValue.textContent = Object.values(numberAndOperatorInput).join(' ');       
+    if (numberAndOperatorInput.firstNumber == '' || 
+        numberAndOperatorInput.operator == '' ||
+        userNumberInput == '') {
+            return;
+    }
+    else {
+        equal();
+        numberAndOperatorInput.operator = buttonValue;
+    }
+}
+
+function backspace() {
+    if (userNumberInput != '') {
+        userNumberInput = userNumberInput.toString().slice(0, -1);
+        if (Object.values(numberAndOperatorInput).every(value => value == '')) {
+            displayValue.textContent = userNumberInput;
+        }
+        else {
+            displayValue.textContent = `${numberAndOperatorInput.firstNumber} ${numberAndOperatorInput.operator} ${userNumberInput}`
+        }
+        return;
+    }
+    else if (numberAndOperatorInput.operator != '') {
+        numberAndOperatorInput.operator = '';
+        userNumberInput = numberAndOperatorInput.firstNumber
+        numberAndOperatorInput.firstNumber = '';
+        displayValue.textContent = userNumberInput;
+        return;
+    }
+    else if (userNumberInput == '' && numberAndOperatorInput.operator == '') {
+        userNumberInput = numberAndOperatorInput.firstNumber;
+        numberAndOperatorInput.firstNumber = '';
+        backspace();
+    }
+}
+
+function clear() {
+    displayValue.textContent = '0';
+    numberAndOperatorInput.firstNumber = '';
+    numberAndOperatorInput.secondNumber = '';
+    numberAndOperatorInput.operator = '';
+    userNumberInput = '';
+}
+
+function decimal() {
+    if (userNumberInput.includes('.') || displayValue.textContent.includes('.')) {
+        alert('Number already contains a decimal');
+        return;
+    }
+    userNumberInput += '.';
+    displayValue.textContent += '.';
+}
+
+function equal() {
+    if (numberAndOperatorInput.firstNumber == '' || 
+        numberAndOperatorInput.operator == '' ||
+        userNumberInput == '' || userNumberInput == '.') {
+            return;
+    }
+    else {
+        numberAndOperatorInput.secondNumber = userNumberInput;
+        result = operateOnValues(numberAndOperatorInput);
+        displayValue.textContent = result;
+        numberAndOperatorInput.firstNumber = result;
+        numberAndOperatorInput.secondNumber = '';
+        numberAndOperatorInput.operator = '';
+        userNumberInput = '';
+    }
+}
+
+function numberInput(buttonValue) {
+    if (userNumberInput == '' && numberAndOperatorInput.operator == '' && numberAndOperatorInput.firstNumber != '') {
+        userNumberInput = numberAndOperatorInput.firstNumber;
+        numberAndOperatorInput.firstNumber = '';
+    }
+    userNumberInput += buttonValue;
+    if (Object.values(numberAndOperatorInput).every(value => value == '')) {
+        displayValue.textContent = userNumberInput;
+    }
+    else {
+        displayValue.textContent = `${numberAndOperatorInput.firstNumber} ${numberAndOperatorInput.operator} ${userNumberInput}`
     }
 }
