2e8d4f04213734f0370e45150b92e85d08987eff
Ezmyrian <56850288+Ezmyrian@users.noreply.github.com>
2025-04-09 18:00:18 -0500

    Target numbers and add event listner to update display

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+let display = document.querySelector('.display');
+let numbers = document.querySelector('.numbers');
+let buttons = numbers.querySelectorAll("button");
+buttons.forEach((button) => button.addEventListener('click', updateDisplay));
+
+function updateDisplay(e) {
+    display.textContent += e.target.textContent;
+}
+
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -11,7 +21,7 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
-    if (num2 == 0) return alert('Nice try, but dividing by zero would destroy the universe...Thanos.')
+    if (num2 == 0) return alert('Nice try, but dividing by zero would destroy the universe...Thanos.');
     return num1 / num2;
 }
 
@@ -25,6 +35,6 @@ function operate(num1, num2, operator) {
             return multiply(num1, num2);
         case '/':
             return divide(num1, num2);
-        default: alert('Error in operate function')
+        default: alert('Error in operate function');
     }
 }
